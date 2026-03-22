282140c4114111554972127c5a8e4497fc2f3ccd
Dirk Lorenz <dirkmlorenz@gmail.com>
2026-03-22 13:20:41 +0100

    Include probes that just started a building in the list of available workers

--- Sharky/Builds/MacroServices/WorkerBuilderService.cs
+++ Sharky/Builds/MacroServices/WorkerBuilderService.cs
@@ -16,9 +16,32 @@
             IEnumerable<UnitCommander> availableWorkers;
             if (workers == null)
             {
-                availableWorkers = ActiveUnitData.Commanders.Values.Where(c => c.UnitCalculation.Unit.UnitType == (uint)UnitTypes.TERRAN_SCV && c.UnitCalculation.Unit.Orders.Any(o => ActiveUnitData.SelfUnits.Values.Any(s => s.Attributes.Contains(SC2APIProtocol.Attribute.Structure) && s.Unit.BuildProgress == 1 && o.TargetWorldSpacePos != null && s.Position.X == o.TargetWorldSpacePos.X && s.Position.Y == o.TargetWorldSpacePos.Y))).Concat(
-                    ActiveUnitData.Commanders.Values.Where(c => c.UnitCalculation.UnitClassifications.HasFlag(UnitClassification.Worker) && !c.UnitCalculation.Unit.BuffIds.Any(b => SharkyUnitData.CarryingResourceBuffs.Contains((Buffs)b))).Where(c => (c.UnitRole == UnitRole.PreBuild || c.UnitRole == UnitRole.None || c.UnitRole == UnitRole.Minerals) && !c.UnitCalculation.Unit.Orders.Any(o => SharkyUnitData.BuildingData.Values.Any(b => (uint)b.Ability == o.AbilityId))))
+                var buildings = ActiveUnitData.SelfUnits.Values.Where(u => u.Attributes.Contains(SC2APIProtocol.Attribute.Structure)).ToArray();
+                availableWorkers = ActiveUnitData.Commanders.Values.Where(c => c.UnitCalculation.Unit.UnitType == (uint)UnitTypes.TERRAN_SCV && c.UnitCalculation.Unit.Orders.Any(o => ActiveUnitData.SelfUnits.Values.Any(s => s.Attributes.Contains(SC2APIProtocol.Attribute.Structure) && s.Unit.BuildProgress == 1 && o.TargetWorldSpacePos != null && s.Position.X == o.TargetWorldSpacePos.X && s.Position.Y == o.TargetWorldSpacePos.Y)));
+                var workersNotCarrying = ActiveUnitData.Commanders.Values.Where(c => c.UnitCalculation.UnitClassifications.HasFlag(UnitClassification.Worker) && !c.UnitCalculation.Unit.BuffIds.Any(b => SharkyUnitData.CarryingResourceBuffs.Contains((Buffs)b)));
+                availableWorkers = availableWorkers.Concat(workersNotCarrying
+                    .Where(c => (c.UnitRole == UnitRole.PreBuild || c.UnitRole == UnitRole.None || c.UnitRole == UnitRole.Minerals || c.UnitRole == UnitRole.Build) && !IsBuilding(c)))
                     .OrderBy(p => Vector2.DistanceSquared(p.UnitCalculation.Position, new Vector2(location.X, location.Y)));
+
+                bool IsBuilding(UnitCommander c)
+                {
+                    var unit = c.UnitCalculation.Unit;
+                    var isProbe = unit.UnitType == (uint) UnitTypes.PROTOSS_PROBE;
+                    return unit.Orders.Any(IsBuildingOrder);
+                    bool HasBuilding(Point l)
+                    {
+                        return l != null && buildings.Any(b => b.Position.X == l.X && b.Position.Y == l.Y);
+                    }
+                    bool IsBuildingOrder(UnitOrder o)
+                    {
+                        if (isProbe && HasBuilding(o.TargetWorldSpacePos))
+                        {
+                            // if there is already a building at the target position the probe is already available again
+                            return false;
+                        }
+                        return SharkyUnitData.BuildingData.Values.Any(b => (uint)b.Ability == o.AbilityId);
+                    }
+                }
             }
             else
             {
