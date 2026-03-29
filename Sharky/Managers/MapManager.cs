ad9623fa613b0304ab36f33734e08e5c0a0c1ffe
Dirk Lorenz <dirkmlorenz@gmail.com>
2026-03-29 16:21:46 +0200

    Improve estimation of footprint sizes for identification of walkable/pathable cells in MapManager and allow it to detect connected components

--- Sharky/Managers/MapManager.cs
+++ Sharky/Managers/MapManager.cs
@@ -2,6 +2,50 @@
 {
     public class MapManager : SharkyManager
     {
+        public record ConnectedComponentInfo
+        {
+            public int[,]? ConnectedComponents { get; set; } = null;
+            public int NumConnectedComponents { get; set; } = 0;
+        }
+
+        private record StructureInfo(int XMin, int XMax, int YMin, int YMax, int[] TouchingComponents)
+        {
+            public static StructureInfo Create(int xMin, int xMax, int yMin, int yMax, int[,]? components)
+            {
+                if (components is null)
+                {
+                    return new StructureInfo(xMin, xMax, yMin, yMax, Array.Empty<int>());
+                }
+                var comps = components!;
+                var nX = comps.GetLength(0);
+                var nY = comps.GetLength(1);
+                var cellsToCheck = Enumerable.Empty<(int, int)>();
+                var startX = Math.Max(0, xMin - 1);
+                var endX = Math.Min(nX - 1, xMax + 1);
+                var countX = endX - startX + 1;
+                var startY = Math.Max(0, yMin - 1);
+                var endY = Math.Min(nY - 1, yMax + 1);
+                var countY = endY - startY + 1;
+                if (xMin > 0)
+                {
+                    cellsToCheck = cellsToCheck.Concat(Enumerable.Range(startY, countY).Select(y => (startX, y)));
+                }
+                if (xMax < nX - 1)
+                {
+                    cellsToCheck = cellsToCheck.Concat(Enumerable.Range(startY, countY).Select(y => (endX, y)));
+                }
+                if (yMin > 0)
+                {
+                    cellsToCheck = cellsToCheck.Concat(Enumerable.Range(startX, countX).Select(x => (x, startY)));
+                }
+                if (yMax < nY - 1)
+                {
+                    cellsToCheck = cellsToCheck.Concat(Enumerable.Range(startX, countX).Select(x => (x, endY)));
+                }
+                return new StructureInfo(xMin, xMax, yMin, yMax, cellsToCheck.Select(xy => components[xy.Item1, xy.Item2]).Where(x => x != 0).Distinct().ToArray());
+            }
+        }
+
         ActiveUnitData ActiveUnitData;
         MapData MapData;
         SharkyUnitData SharkyUnitData;
@@ -11,8 +55,13 @@
 
         private int LastUpdateFrame;
         private readonly int FramesPerUpdate;
+        private readonly HashSet<float> UnexpectedMineralSizes = new();
+        private readonly Dictionary<UnitTypes, int> EstimatedFootPrintSizes = new();
+        private readonly ConnectedComponentInfo CCInfo = new();
+        private StructureInfo?[,] StructureInfos { get; set; } = null;
 
         public bool FullVisionMode { get; set; } = false;
+        public bool DoUpdateConnectedComponents { get; set; } = false;
 
         public MapManager(MapData mapData, ActiveUnitData activeUnitData, SharkyOptions sharkyOptions, SharkyUnitData sharkyUnitData, DebugService debugService, WallDataService wallDataService)
         {
@@ -68,6 +117,11 @@
             UpdateInEnemyVision();
             UpdateNumberOfAllies((int)observation.Observation.GameLoop);
             UpdatePathBlocked();
+            if (DoUpdateConnectedComponents)
+            {
+                UpdateConnectedComponents();
+                UpdateStructureInfos();
+            }
 
             return null;
         }
@@ -199,6 +253,10 @@
             }
         }
 
+        private bool BlocksPath(UnitCalculation uc) => !uc.Unit.IsFlying && uc.Attributes.Contains(SC2APIProtocol.Attribute.Structure) && uc.Unit.UnitType != (uint)UnitTypes.TERRAN_SUPPLYDEPOTLOWERED;
+
+        private IEnumerable<UnitCalculation> PathBlockers() => ActiveUnitData.EnemyUnits.Values.Concat(ActiveUnitData.SelfUnits.Values).Concat(ActiveUnitData.NeutralUnits.Values).Where(BlocksPath);
+
         void UpdatePathBlocked()
         {
             for (var x = 0; x < MapData.MapWidth; x++)
@@ -209,27 +267,9 @@
                 }
             }
 
-            foreach (var enemy in ActiveUnitData.EnemyUnits.Where(e => !e.Value.Unit.IsFlying && e.Value.Attributes.Contains(SC2APIProtocol.Attribute.Structure) && e.Value.Unit.UnitType != (uint)UnitTypes.TERRAN_SUPPLYDEPOTLOWERED))
+            foreach (var uc in PathBlockers())
             {
-                var nodes = GetNodesInRange(enemy.Value.Unit.Pos, enemy.Value.Unit.Radius, MapData.MapWidth, MapData.MapHeight);
-                foreach (var node in nodes)
-                {
-                    MapData.Map[(int)node.X,(int)node.Y].PathBlocked = true;
-                }
-            }
-
-            foreach (var enemy in ActiveUnitData.SelfUnits.Where(e => !e.Value.Unit.IsFlying && e.Value.Attributes.Contains(SC2APIProtocol.Attribute.Structure) && e.Value.Unit.UnitType != (uint)UnitTypes.TERRAN_SUPPLYDEPOTLOWERED))
-            {
-                var nodes = GetNodesInRange(enemy.Value.Unit.Pos, enemy.Value.Unit.Radius, MapData.MapWidth, MapData.MapHeight);
-                foreach (var node in nodes)
-                {
-                    MapData.Map[(int)node.X,(int)node.Y].PathBlocked = true;
-                }
-            }
-
-            foreach (var enemy in ActiveUnitData.NeutralUnits.Where(e => !e.Value.Unit.IsFlying && e.Value.Attributes.Contains(SC2APIProtocol.Attribute.Structure) && e.Value.Unit.UnitType != (uint)UnitTypes.TERRAN_SUPPLYDEPOTLOWERED))
-            {
-                var nodes = GetNodesInRange(enemy.Value.Unit.Pos, enemy.Value.Unit.Radius, MapData.MapWidth, MapData.MapHeight);
+                var nodes = GetNodesInFootPrint(uc.Unit, MapData.MapWidth, MapData.MapHeight);
                 foreach (var node in nodes)
                 {
                     MapData.Map[(int)node.X,(int)node.Y].PathBlocked = true;
@@ -377,6 +417,123 @@
             return nodes;
         }
 
+        private (int xMin, int xMax, int yMin, int yMax) GetFootPrintNodeRange(Unit unit)
+        {
+            /// In the SC2APIProtocol, the unit radius (how big it is for combat/collisions) is different from its footprint (the square grid cells it occupies on the map).
+            /// To get the footprint, you don't look at the Unit itself; you look at the UnitTypeData inside the ResponseData (usually requested once at the start of the game via RequestData).
+            /// 1. The footprint Field
+            /// Each UnitTypeData message contains a footprint field (string). This string refers to a specific shape defined in the game's balance files (e.g., Footprint3x3, Footprint4x4Contour).
+            /// Town Halls (Nexus/CC/Hatchery): Usually a 5x5 footprint.
+            /// Supply Depots/Pylons: Usually a 2x2 footprint.
+            /// Barracks/Gateways: Usually a 3x3 footprint.
+            /// Mineral Patches: Usually a 2x1 (horizontal) footprint.
+            /// 2. Converting Footprint to Grid Cells
+            /// Since footprints are usually square or rectangular, you don't use a "radius" (which is circular). Instead, you use the center coordinates of the unit and "block out" the grid cells:
+            /// If the footprint is Odd (3x3, 5x5): The unit's pos (x, y) will be on an integer (e.g., 10.0, 20.0). You block out the cells from pos.x - 1 to pos.x + 1 for a 3x3.
+            /// If the footprint is Even (2x2, 4x4): The unit's pos (x, y) will be on a half-integer (e.g., 10.5, 20.5). You block out the cells by rounding down/up.
+            /// 3. Quick Reference Table
+            /// Unit Type	Footprint Size	Radius (Combat)
+            /// Town Hall	5x5	2.75
+            /// Mineral Patch	2x1	1.125
+            /// Vespene Geyser	3x3	1.75
+            /// Supply Depot	2x2	1.0
+            /// Barracks	3x3	1.5            
+
+            var position = unit.Pos;
+            var radius = unit.Radius;
+            var ut = (UnitTypes)unit.UnitType;
+            SharkyUnitData.BuildingData.TryGetValue(ut, out var buildingTypeData);
+
+            if (buildingTypeData is not null)
+            {
+                return NByN(buildingTypeData.Size);
+            }
+            if (SharkyUnitData.GasGeyserTypes.Contains(ut) || SharkyUnitData.GasGeyserRefineryTypes.Contains(ut))
+            {
+                return NByN(3);
+            }
+            if (SharkyUnitData.MineralFieldTypes.Contains(ut))
+            {
+                if (radius != 1.125)
+                {
+                    if (!UnexpectedMineralSizes.Contains(radius))
+                    {
+                        Console.WriteLine($"MapManager: Unexpectedly sized mineral patch with radius of {radius}");
+                        UnexpectedMineralSizes.Add(radius);
+                    }
+                }
+                // we assume that mineral fields are horizontal - in principle they can be rotated
+                var x = (int)Math.Round(position.X);
+                var y = (int)Math.Round(position.Y - 0.5);
+                return (x - 1, x, y, y);
+            }
+            if (radius == 1.125) // e.g. unbuildable plates
+            {
+                return NByN(2);
+            }
+            if (radius == 1.8125) // e.g. warp gate
+            {
+                return NByN(3);
+            }
+            if (radius == 2.75)
+            {
+                return NByN(5);
+            }
+            if (radius == 3.1875) // e.g. large destructible rocks or tentacle rocks
+            {
+                return NByN(6);
+            }
+            if (!EstimatedFootPrintSizes.TryGetValue(ut, out int estimatedFootPrintSize))
+            {
+                estimatedFootPrintSize = Math.Max(1, (int)Math.Floor(2 * radius));
+                EstimatedFootPrintSizes[ut] = estimatedFootPrintSize;
+                Console.WriteLine($"Map Manager: Estimated foot print size for structure {unit} with radius {radius} as {estimatedFootPrintSize}");
+            }
+            return NByN(estimatedFootPrintSize);
+
+            (int, int, int, int) NByN(int n)
+            {
+                var steps = Math.DivRem(n, 2, out var remainder);
+                var centerOffSet = remainder == 0 ? 0f : 0.5f;
+                var x = (int)Math.Round(position.X - centerOffSet);
+                var y = (int)Math.Round(position.Y - centerOffSet);
+                return (x - steps, x + steps + remainder - 1, y - steps, y + steps + remainder - 1);
+            }
+        }
+
+        private List<Vector2> GetNodesInFootPrint(Unit unit, int columns, int rows)
+        {
+            var nodes = new List<Vector2>();
+            var (xMin, xMax, yMin, yMax) = GetFootPrintNodeRange(unit);
+
+            if (xMin < 0)
+            {
+                xMin = 0;
+            }
+            if (xMax >= columns)
+            {
+                xMax = columns - 1;
+            }
+            if (yMin < 0)
+            {
+                yMin = 0;
+            }
+            if (yMax >= rows)
+            {
+                yMax = rows - 1;
+            }
+
+            for (int x = xMin; x <= xMax; x++)
+            {
+                for (int y = yMin; y <= yMax; y++)
+                {
+                    nodes.Add(new Vector2(x, y));
+                }
+            }
+
+            return nodes;
+        }
+
         void UpdateCreep(ImageData creep)
         {
             for (var x = 0; x < creep.Size.X; x++)
@@ -396,9 +553,10 @@
                 {
                     for (var y = 0; y < visiblilityMap.Size.Y; y++)
                     {
-                        MapData.Map[x, y].InSelfVision = true;
-                        MapData.Map[x, y].Visibility = 2;// 2 is fully visible
-                        MapData.Map[x, y].LastFrameVisibility = frame;
+                        var cell = MapData.Map[x, y];
+                        cell.InSelfVision = true;
+                        cell.Visibility = 2;// 2 is fully visible
+                        cell.LastFrameVisibility = frame;
                     }
                 }
                 return;
@@ -408,11 +566,14 @@
             {
                 for (var y = 0; y < visiblilityMap.Size.Y; y++)
                 {
-                    MapData.Map[x,y].InSelfVision = GetDataValueByte(visiblilityMap, x, y) == 2; // 2 is fully visible
-                    MapData.Map[x,y].Visibility = GetDataValueByte(visiblilityMap, x, y);
-                    if (GetDataValueByte(visiblilityMap, x, y) == 2)
+                    var cell = MapData.Map[x, y];
+                    var visibility = GetDataValueByte(visiblilityMap, x, y);
+                    var fullyVisible = visibility == 2; // 2 is fully visible
+                    cell.InSelfVision = fullyVisible;
+                    cell.Visibility = visibility;
+                    if (fullyVisible)
                     {
-                        MapData.Map[x,y].LastFrameVisibility = frame;
+                        cell.LastFrameVisibility = frame;
                     }
                 }
             }
@@ -429,6 +590,170 @@
         {
             int pixelID = x + y * data.Size.X;
             return data.Data[pixelID];
+        }
+
+        private void UpdateConnectedComponents()
+        {
+            var map = MapData.Map;
+            var nX = MapData.MapWidth;
+            var nY = MapData.MapHeight;
+            CCInfo.ConnectedComponents ??= new int[nX, nY];
+            var components = CCInfo.ConnectedComponents!;
+            Array.Clear(components);
+            var component = 0;
+            var dr = new int[] { -1, 1, 0, 0, -1, -1, 1, 1 };
+            var dc = new int[] { 0, 0, -1, 1, -1, 1, -1, 1 };
+
+            for (int i = 0; i < nX; i++)
+            {
+                for (int j = 0; j < nY; j++)
+                {
+                    if (!IsWalkable(i, j) || components[i, j] != 0)
+                        continue;
+
+                    component++;
+
+                    Queue<(int r, int c)> queue = new();
+                    queue.Enqueue((i, j));
+                    components[i, j] = component;
+
+                    while (queue.Count > 0)
+                    {
+                        var (cr, cc) = queue.Dequeue();
+
+                        for (int n = 0; n < 4; n++)
+                        {
+                            var nr = cr + dr[n];
+                            var nc = cc + dc[n];
+
+                            if (nr < 0 || nc < 0 || nr >= nX || nc >= nY)
+                                continue;
+
+                            if (!IsWalkable(nr, nc) || components[nr, nc] != 0)
+                                continue;
+
+                            components[nr, nc] = component;
+                            queue.Enqueue((nr, nc));
+                        }
+                    }
+                }
+            }
+            if (component != CCInfo.NumConnectedComponents)
+            {
+                if (CCInfo.NumConnectedComponents != 0)
+                {
+                    //TagService.Tag($"ncc_{NumConnectedComponents}_{component}");
+                }
+                CCInfo.NumConnectedComponents = component;
+            }
+
+            bool IsWalkable(int x, int y)
+            {
+                var cell = map[x, y];
+                return cell.Walkable && !cell.PathBlocked;
+            }
+        }
+
+        private void UpdateStructureInfos()
+        {
+            if (CCInfo.ConnectedComponents is null)
+            {
+                return;
+            }
+            var nX = MapData.MapWidth;
+            var nY = MapData.MapHeight;
+            StructureInfos ??= new StructureInfo[nX, nY];
+            Array.Clear(StructureInfos);
+            var sis = StructureInfos!;
+            var components = CCInfo.ConnectedComponents!;
+            foreach (var uc in PathBlockers())
+            {
+                var (xMin, xMax, yMin, yMax) = GetFootPrintNodeRange(uc.Unit);
+                var structureInfo = StructureInfo.Create(xMin, xMax, yMin, yMax, components);
+                xMin = Math.Max(0, xMin);
+                xMax = Math.Min(nX - 1, xMax);
+                yMin = Math.Max(0, yMin);
+                yMax = Math.Min(nY  - 1, yMax);
+                for (int x = xMin; x <= xMax; x++)
+                {
+                    for (int y = yMin; y <= yMax; y++)
+                    {
+                        sis[x, y] = structureInfo;
+                    }
+                }
+            }
+        }
+
+        public ConnectedComponentInfo GetConnectedComponentInfo()
+        {
+            return CCInfo;
+        }
+
+        public int[] GetConnectedComponents(int x, int y)
+        {
+            if (CCInfo.ConnectedComponents is null || x < 0 || y < 0 || x >= MapData.MapWidth || y >= MapData.MapHeight)
+            {
+                return Array.Empty<int>();
+            }
+            var component = CCInfo.ConnectedComponents[x, y];
+            if (component != 0)
+            {
+                return new int[] { component };
+            }
+            if (StructureInfos is null)
+            {
+                return Array.Empty<int>();
+            }
+            var structureInfo = StructureInfos[x, y];
+            return structureInfo is null ? Array.Empty<int>() : structureInfo.TouchingComponents;
+        }
+
+        public int[] GetConnectedComponents(float x, float y) => GetConnectedComponents((int)x, (int)y);
+        public int[] GetConnectedComponents(Vector2 pos) => GetConnectedComponents(pos.X, pos.Y);
+        public int[] GetConnectedComponents(UnitCalculation uc) => GetConnectedComponents(uc.Position);
+        public int[] GetConnectedComponents(SC2APIProtocol.Point p) => GetConnectedComponents(p.X, p.Y);
+
+        public int[] GetConnectedComponentsByUnitTag(ulong  unitTag)
+        {
+            if (ActiveUnitData.NeutralUnits.TryGetValue(unitTag, out var n))
+            {
+                return GetConnectedComponents(n);
+            }
+            if (ActiveUnitData.EnemyUnits.TryGetValue(unitTag, out var e))
+            {
+                return GetConnectedComponents(e);
+            }
+            if (ActiveUnitData.SelfUnits.TryGetValue(unitTag, out var s))
+            {
+                return GetConnectedComponents(s);
+            }
+            return Array.Empty<int>();
+        }
+
+        public bool IsCrossingComponents(UnitCalculation uc)
+        {
+            var orders = uc.Unit.Orders;
+            if (orders.Count == 0)
+            {
+                return false;
+            }
+            var components = GetConnectedComponents(uc);
+            if (components.Length != 1)
+            {
+                return false;
+            }
+            var component = components[0];
+            foreach (var o in uc.Unit.Orders)
+            {
+                var targetComponents = o.TargetCase switch
+                {
+                    UnitOrder.TargetOneofCase.TargetWorldSpacePos => GetConnectedComponents(o.TargetWorldSpacePos),
+                    UnitOrder.TargetOneofCase.TargetUnitTag => GetConnectedComponentsByUnitTag(o.TargetUnitTag),
+                    _ => Array.Empty<int>()
+                };
+                return targetComponents.Length > 0 && !targetComponents.Contains(component);
+            }
+            return false;
         }
     }
 }
